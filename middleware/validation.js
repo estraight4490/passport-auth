f8138262d23f58d6793aa02ca6505a377f698003
Defacto <defacto@Defactos-MacBook-Pro.local>
2022-10-14 08:42:26 -0400

    lint

--- middleware/validation.js
+++ middleware/validation.js
@@ -3,15 +3,15 @@ const { check, validationResult } = require("express-validator");
 
 const validate_registration = [
   // Check Email
-  check("email", "Must be a proper email address.").isEmail()
-  .trim().escape().normalizeEmail(),
+  check("email", "Must be a proper email address.").isEmail().
+    trim().escape().normalizeEmail(),
   // Check Password
-  check("password").isLength({ min: 8 }).withMessage("Password Must Be at Least 8 Characters")
-  .matches("[0-9]").withMessage("Password Must Contain a Number").matches("[A-Z]")
-  .withMessage("Password Must Contain an Uppercase Letter").trim().escape(),
+  check("password").isLength({ min: 8 }).withMessage("Password Must Be at Least 8 Characters").
+    matches("[0-9]").withMessage("Password Must Contain a Number").matches("[A-Z]").
+    withMessage("Password Must Contain an Uppercase Letter").trim().escape(),
   // Check Name
-  check("name", "Name must contain at least two characters.").isLength({ min: 2 })
-  .matches("[A-Z]").trim().escape(),
+  check("name", "Name must contain at least two characters.").isLength({ min: 2 }).
+    matches("[A-Z]").trim().escape(),
 ];
 
 module.exports.validationResult = validationResult;
